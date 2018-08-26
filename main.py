d1296dccf1da561c5283695e6c720cf87bcd6bc8
Dan <dans.address@outlook.com>
2018-08-26 21:06:40 +0000

    Implement new BasicTracer child class

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 
 
 def main():
-    Tracer('https://httpbin.org/redirect/4')
+    BasicTracer('https://httpbin.org/redirect/4')
 
 
 class Tracer:
@@ -17,33 +17,48 @@ class Tracer:
         resp = requests.get(self.url)
         return self.format_response(resp)
 
-    def format_response(self, resp):
-        if resp.history:
-            for redirects in resp.history:
-                self.template(status_code=redirects.status_code,
-                              http_version=redirects.raw.version,
-                              request_type=redirects.request.method,
-                              url=redirects.url, time=redirects.elapsed,
-                              cookies='cookies if any')
-            print(resp.status_code, resp.request.method,
-                  resp.url, resp.elapsed, 'cookies if any')
-            print(f"Number of hops: {len(resp.history) +1}")
-
-    def template(self, status_code, http_version, request_type, url, time, cookies):
-        template = f"[{status_code}] HTTP/{http_version} {request_type} {url} {time} {cookies}"
+    def template(self, status_code, http_version, request_type, url, time,
+                 cookies):
+        template = f"[{status_code}] HTTP/{http_version} {request_type} {url} ({time}ms) ({cookies} found)"
         print(template)
 
     def time_converter(self, resp):
         """Parses request.elapsed into milliseconds"""
-        pass
+        return int(resp.total_seconds() * 1000)
 
     def http_version_converter(self, resp):
         """Reformat requests raw.version integer into the HTTP header format"""
+        if len(str(resp)) > 1:
+            resp = [x for x in str(resp)]
+            return f"{resp[0]}.{resp[1]}"
+        else:
+            return resp
+
+    def total_time_elapsed(self, resp):
         pass
 
     def cookies_exist(self, resp):
         """Check if cookies sent during response."""
         pass
+
+
+class BasicTracer(Tracer):
+
+    def format_response(self, resp):
+        if resp.history:
+            for redirects in resp.history:
+                self.template(status_code=redirects.status_code,
+                              http_version=self.http_version_converter(redirects.raw.version),
+                              request_type=redirects.request.method,
+                              url=redirects.url, time=self.time_converter(redirects.elapsed),
+                              cookies='cookies if any')
+            print(resp.status_code, resp.request.method, resp.raw.version,
+                  resp.url, resp.elapsed, 'cookies if any')
+            print(f"Number of hops: {len(resp.history) +1}")
+
+
+class FullTracer(Tracer):
+    pass
 
 
 def args_parser():
