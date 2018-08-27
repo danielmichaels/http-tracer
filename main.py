48637157bebece0b001a5f858ea02519f6e2c6e0
Dan <dans.address@outlook.com>
2018-08-27 21:46:09 +0000

    add click support, move format_response func into Tracer parent class

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 
+import click
 import requests
 
 
-def main():
-    BasicTracer('https://httpbin.org/redirect/4')
-    BasicTracer('http://nyti.ms/1QETHgV')  # many redirects
+@click.command()
+@click.argument('url')
+@click.option('--full', '-f', is_flag=True,
+              help='Do a full scan of the redirect chain.')
+def main(url, full):
+    tracer = BasicTracer(url)
+    resp = tracer.get_response()
+    if full:
+        full_tracer = FullTracer(url)
+        full_tracer.format_response(resp)
+        full_tracer.full_tracer_formate_placeholder(resp)
+    else:
+        tracer.format_response(resp)
+    # BasicTracer('https://httpbin.org/redirect/4')
+    # BasicTracer('http://nyti.ms/1QETHgV')  # many redirects
 
 
 class Tracer:
@@ -18,7 +31,8 @@ class Tracer:
 
     def get_response(self):
         resp = requests.get(self.url)
-        return self.format_response(resp)
+        # return self.format_response(resp)
+        return resp
 
     def template(self, status_code, http_version, request_type, url, time,
                  cookies):
@@ -55,45 +69,41 @@ class Tracer:
             if len(cookies) >= 1:
                 return f"(cookies: {len(cookies)})"
 
-
-
-class BasicTracer(Tracer):
-    """The basic output of http-tracer."""
     def format_response(self, resp):
         if resp.history:
             for redirects in resp.history:
                 self.template(status_code=redirects.status_code,
-                              http_version=self.http_version_converter(redirects.raw.version),
+                              http_version=self.http_version_converter(
+                                  redirects.raw.version),
                               request_type=redirects.request.method,
-                              url=redirects.url, time=self.time_converter(redirects.elapsed),
+                              url=redirects.url,
+                              time=self.time_converter(redirects.elapsed),
                               cookies=self.cookies_exist(redirects))
-            self.template(resp.status_code, resp.request.method,
+            self.template(resp.status_code,
                           self.http_version_converter(resp.raw.version),
+                          resp.request.method,
                           resp.url, self.time_converter(resp.elapsed),
-                          'cookies if any')
-            # print(f"Number of hops: {len(resp.history) +1} and took {self.total_time_elapsed(resp)}ms")
-            print(
-                f"HTTP-Tracer finished in {self.total_time_elapsed(resp)}ms over {len(resp.history) + 1} hops")
+                          self.cookies_exist(resp))
+        print(
+            f"HTTP-Tracer finished in {self.total_time_elapsed(resp)}ms over {len(resp.history) + 1} hops")
 
 
-class FullTracer(Tracer):
+class BasicTracer(Tracer):
+    """The basic output of http-tracer."""
+    print('basic tracer init')
+    pass
+
+
+class FullTracer(BasicTracer):
     """Full output that presents headers, cookies, cert validation/ expiry."""
-    pass
 
-
-def args_parser():
-    """
-    should look like this:
-
-    python http-tracer.py <url>
-
-    <url> is a required argument
-
-    which will call Tracer(<url>)
-    :return:
-    """
-    pass
-
+    def full_tracer_formate_placeholder(self, resp):
+        print('full tracer')
+        print(resp.history)
+        print("""
+        this is where each hops headers, cookies, certificate validations etc
+        will be printed.""")
+        print("click.pager to parse using less as an option boolean")
 
 if __name__ == '__main__':
     main()
