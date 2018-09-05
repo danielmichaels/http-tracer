a5bfa98d1bfcb5ff88fe7ca0a7d1728d20f0a694
Dan <dans.address@outlook.com>
2018-09-05 15:32:28 +0000

    If 404, will report to user.

--- main.py
+++ main.py
@@ -24,6 +24,13 @@ def main(url, full):
 class Tracer:
     """Parent class that gets the url response, and contains the helper methods
     for cleaning up the formatting."""
+    logo = f"""{fg.WHITE}
+  _  _ _   _            _                       
+ | || | |_| |_ _ __ ___| |_ _ _ __ _ __ ___ _ _ 
+ | __ |  _|  _| '_ \___|  _| '_/ _` / _/ -_) '_|
+ |_||_|\__|\__| .__/    \__|_| \__,_\__\___|_|  
+              |_|                              v {version}{sty.RESET_ALL}
+    """
 
     def __init__(self, url):
         self.url = url
@@ -32,11 +39,9 @@ class Tracer:
     def get_response(self):
         try:
             resp = requests.get(self.url)
-            if resp.status_code == 200:
-                return resp
-            else:
-                raise ConnectionError
-
+            return resp
+        except ConnectionError as e:
+            click.secho(f"{e} Caused Fatal Error!", blink=True, fg='red')
         except requests.ConnectionError or requests.ConnectTimeout as e:
             click.secho(f"{e} Caused Fatal Error!", blink=True, fg='red')
         except requests.HTTPError as e:
@@ -80,13 +85,9 @@ class Tracer:
                 return f"(cookies: {len(cookies)})"
 
     def format_response(self, resp):
+        print(self.logo)
         if resp.history:
-            print(f"""{fg.WHITE}  _  _ _   _            _                       
- | || | |_| |_ _ __ ___| |_ _ _ __ _ __ ___ _ _ 
- | __ |  _|  _| '_ \___|  _| '_/ _` / _/ -_) '_|
- |_||_|\__|\__| .__/    \__|_| \__,_\__\___|_|  
-              |_|                              v {version} 
-""")
+
             for redirects in resp.history:
                 self.template(status_code=redirects.status_code,
                               http_version=self.http_version_converter(
@@ -95,14 +96,21 @@ class Tracer:
                               url=redirects.url,
                               time=self.time_converter(redirects.elapsed),
                               cookies=self.cookies_exist(redirects))
-            self.template(resp.status_code,
-                          self.http_version_converter(resp.raw.version),
-                          resp.request.method,
-                          resp.url, self.time_converter(resp.elapsed),
-                          self.cookies_exist(resp))
-        print(
-            f"\n{fg.WHITE}HTTP-Tracer finished in {fg.CYAN}{self.total_time_elapsed(resp)}ms{fg.WHITE} over"
-            f"{fg.CYAN} {len(resp.history) + 1}{fg.WHITE} hops{sty.RESET_ALL}")
+        self.template(resp.status_code,
+                      self.http_version_converter(resp.raw.version),
+                      resp.request.method,
+                      resp.url, self.time_converter(resp.elapsed),
+                      self.cookies_exist(resp))
+        if resp.status_code == 404:
+            print(
+                f"\n{fg.WHITE}HTTP-Tracer Returned {fg.RED}404{sty.RESET_ALL}{fg.WHITE}"
+                f" in {fg.CYAN}{self.total_time_elapsed(resp)}ms{fg.WHITE} over"
+                f"{fg.CYAN} {len(resp.history) + 1}{fg.WHITE} hops{sty.RESET_ALL}")
+
+        else:
+            print(
+                f"\n{fg.WHITE}HTTP-Tracer finished in {fg.CYAN}{self.total_time_elapsed(resp)}ms{fg.WHITE} over"
+                f"{fg.CYAN} {len(resp.history) + 1}{fg.WHITE} hops{sty.RESET_ALL}")
 
 
 class FullTracer(Tracer):
@@ -135,13 +143,12 @@ class FullTracer(Tracer):
         for dict_item in header_list:
             hop += 1
             print()
-            click.secho(f"######### HOP NUMBER: {hop} ##########",
+            click.secho(f"********* HOP NUMBER: {hop} **********",
                         fg='magenta')
             print()
             click.secho("##################################", fg='yellow')
             print("             HEADERS              ")
             click.secho("##################################", fg='yellow')
-            # click.secho(f"    [*]hop number: {hop}[*]", fg='white')
             print()
 
             for k, v in dict_item.items():
@@ -153,7 +160,9 @@ class FullTracer(Tracer):
                 click.secho("##################################", fg='green')
                 print("             COOKIES              ")
                 click.secho("##################################", fg='green')
-                print(f"Cookie: {dict_item['Set-Cookie']}")
+                print()
+                print(
+                    f"{fg.WHITE}Cookie:{sty.RESET_ALL} {dict_item['Set-Cookie']}")
                 print()
 
             if 'Location' in dict_item.keys():
@@ -161,13 +170,15 @@ class FullTracer(Tracer):
                 click.secho("##################################", fg='blue')
                 print("             REDIRECTION              ")
                 click.secho("##################################", fg='blue')
-                print(f"{fg.WHITE} Request for:{sty.RESET_ALL} {resp.url}")
+                print()
+                print(f"{fg.WHITE}Request for:{sty.RESET_ALL} {resp.url}")
                 print(
-                    f"{fg.WHITE} Redirected to{sty.RESET_ALL} {dict_item['Location']}")
+                    f"{fg.WHITE}Redirected to{sty.RESET_ALL} {dict_item['Location']}")
 
         print()
-        click.secho(f"Final URL: {resp.url} [{resp.status_code}]",
-                    fg='magenta')
+        print(f"{fg.YELLOW}!! FINAL DESTINATION !!")
+        print(f"{fg.WHITE}URL:{sty.RESET_ALL} {resp.url}")
+        print(f"{fg.WHITE}Status Code:{sty.RESET_ALL} {resp.status_code}")
 
 
 if __name__ == '__main__':
