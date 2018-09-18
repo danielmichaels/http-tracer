1d75e3e03bd757f230c12ead50a91e7de798fe1a
Dan <dans.address@outlook.com>
2018-09-18 12:45:41 +0000

    Create test suite package
    notes:
    + no tests setup yet
    + includes context for running unittests relative directory

---
+++ tests/context.py
@@ -0,0 +1,7 @@
+"""context helper module to allow unittests to import tracer package."""
+import sys
+
+import os
+
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
---
+++ tests/test_basic.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+class TestConnections(unittest.TestCase):
+    """Basic testing suite"""
+
+    def test_response(self):
+        pass
+
+    def test_time_converter(self):
+        pass
+
+    def test_version_converter(self):
+        pass
+
+    def test_total_time_elapsed_calc(self):
+        pass
+
+    def test_cookies_exist(self):
+        pass
+
+    def test_create_dicts(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
